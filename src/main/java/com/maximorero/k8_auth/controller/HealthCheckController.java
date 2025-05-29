d1a7846e94bc736327a721bba22c7f9478d6a95c
maximilianomorero <maximiliano.morero@localpayment.com>
2025-05-29 08:53:32 +0200

    test_fail

--- src/main/java/com/maximorero/k8_auth/controller/HealthCheckController.java
+++ src/main/java/com/maximorero/k8_auth/controller/HealthCheckController.java
@@ -21,7 +21,7 @@ public class HealthCheckController {
 	public ResponseEntity<JsonNode> ping() throws Exception {
 		log.info("[log_name: ping]");
 		ObjectMapper mapper = new ObjectMapper();
-		JsonNode response = mapper.createObjectNode().put("message", "pong_k8_auth");
+		JsonNode response = mapper.createObjectNode().put("message", "pong_k8_auth_test_fail");
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
