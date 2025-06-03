1943ffc54d0bd8e75fe2ca7e0a07ca1b72f5b32a
maximilianomorero <maximiliano.morero@localpayment.com>
2025-06-03 09:29:15 +0200

    test

--- src/test/java/com/maximorero/k8_auth/controller/HealthCheckControllerTest.java
+++ src/test/java/com/maximorero/k8_auth/controller/HealthCheckControllerTest.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(HealthCheckController.class)
 public class HealthCheckControllerTest {
 
@@ -30,5 +33,5 @@ public void shouldReturnNoExceptionMessageWhenThrowIsFalse() throws Exception {
                     .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.exception").value("No se lanzó ninguna excepción."));
-}
+    }
 }
