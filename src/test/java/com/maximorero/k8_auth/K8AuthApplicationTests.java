598d892e6beb8dd937a8cc02c5e8f8fd7a278f98
maximilianomorero <maximiliano.morero@localpayment.com>
2025-06-05 14:29:33 +0200

    gitops

--- src/test/java/com/maximorero/k8_auth/K8AuthApplicationTests.java
+++ src/test/java/com/maximorero/k8_auth/K8AuthApplicationTests.java
@@ -3,13 +3,17 @@ package com.maximorero.k8_auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class K8AuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private RestTemplate restTemplate;
 
+    @Test
+    void contextLoads() {
+    }
 }
