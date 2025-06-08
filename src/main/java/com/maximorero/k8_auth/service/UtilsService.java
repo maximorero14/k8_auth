5fce2492a23dcb89b2f4a063b947916241bfc610
maximilianomorero <maximiliano.morero@localpayment.com>
2025-06-08 12:41:09 +0200

    tets

---
+++ src/main/java/com/maximorero/k8_auth/service/UtilsService.java
@@ -0,0 +1,19 @@
+package com.maximorero.k8_auth.service;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtilsService {
+    public String getStackTraceAsString(Throwable throwable) {
+        if(throwable == null || throwable.getStackTrace().length == 0){
+            return "";
+        }
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        throwable.printStackTrace(printWriter);
+        return stringWriter.toString().replaceAll(System.lineSeparator(), " ");
+    }
+}
