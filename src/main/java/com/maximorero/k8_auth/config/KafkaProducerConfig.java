91c0c8714edf1a61f9ad2316439c726af622b96b
maximilianomorero <maximiliano.morero@localpayment.com>
2025-06-13 12:14:24 +0200

    tets

--- src/main/java/com/maximorero/k8_auth/config/KafkaProducerConfig.java
+++ src/main/java/com/maximorero/k8_auth/config/KafkaProducerConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value("${kafka.bootstrap-servers}")
+    @Value("${spring.kafka.producer.bootstrap-servers}")
     private String bootstrapServers;
 
     @Bean
